e4dd0542cb626096ff1d52745d92cf409fb232d2
Wei Zhang <awzhan@amazon.com>
2025-05-11 19:50:51 -0700

    added move control

--- src/main/java/com/awzhan/game/model/SnakeHead.java
+++ src/main/java/com/awzhan/game/model/SnakeHead.java
@@ -1,11 +1,14 @@
 package com.awzhan.game.model;
 
 import com.awzhan.game.GameWin;
+import com.awzhan.game.util.GameUtils;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 @Getter
 @Setter
@@ -14,6 +17,44 @@ public class SnakeHead extends Snake {
 
     public SnakeHead(Image image, int x, int y, GameWin gameWin) {
         super(image, x, y, gameWin);
+        this.gameWin.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent event) {
+                changeDirection(event);
+            }
+        });
+    }
+
+    private void changeDirection(KeyEvent event) {
+        System.out.println(event.getKeyCode());
+        switch (event.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                if (!"down".equalsIgnoreCase(direction)) {
+                    direction = "up";
+                    image = GameUtils.snakeHeadUp;
+                }
+                break;
+            case KeyEvent.VK_DOWN:
+                if (!"up".equalsIgnoreCase(direction)) {
+                    direction = "down";
+                    image = GameUtils.snakeHeadDown;
+                }
+                break;
+            case KeyEvent.VK_LEFT:
+                if (!"right".equalsIgnoreCase(direction)) {
+                    direction = "left";
+                    image = GameUtils.snakeHeadLeft;
+                }
+                break;
+            case KeyEvent.VK_RIGHT:
+                if (!"left".equalsIgnoreCase(direction)) {
+                    direction = "right";
+                    image = GameUtils.snakeHeadRight;
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     public void move() {
